f4cfc7122ae7af52e91a2f5990d9a3e669e17c23
mac <mac@macs-MacBook-Pro.local>
2025-10-16 10:48:00 +0500

    problem14

--- api/hello.ts
+++
@@ -1,8 +0,0 @@
-export default function handler(req: any, res: any) {
-  if (req.method === 'GET') {
-    res.status(200).json({ ok: true, message: 'Hello from API' })
-  } else {
-    res.setHeader('Allow', 'GET')
-    res.status(405).end('Method Not Allowed')
-  }
-}
